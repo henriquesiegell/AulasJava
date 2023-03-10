267e0ca21d44d776507e30f05dddf84bc775ac11
Henrique Siegel <henriqueweirichmeurersiegel@gmail.com>
2023-03-10 12:46:03 -0300

    Upload inicial

---
+++ AulasJava/src/Aulas/B_Variaveis.java
@@ -0,0 +1,29 @@
+package Aulas;
+
+public class B_Variaveis {
+
+	public static void main(String[] args) {
+		int quantidade ; // "int" é usada para declarar variaveis com números inteiros
+		quantidade = 15; // atribuindo o valor 15.
+		
+		System.out.println(quantidade);
+		
+		quantidade = 10; // atribuindo um novo valor à variavel
+		
+		System.out.println(quantidade);
+		
+// ------------------------------------------------------------------------------------------------
+		
+	//	int 2alunos; // Não pode começar com número
+	//	int quantidade alunos; // Não pode ter espaços
+	//	int new; // Não pode ter palavras reservadas
+
+//-------------------------------------------------------------------------------------------------
+	
+		int quantidadeAlunos;  // Forma padrão de escrever variáveis
+		int quantidadeAlunosNota10;
+		int numeroAlunosAprovados;
+
+	}
+
+}
---
+++ AulasJava/src/Exercicios/A_OlaMundo.java
@@ -0,0 +1,12 @@
+package Exercicios;
+
+public class A_OlaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro codigo em \"Java\" feito por mim mesmo :)\n");
+		System.out.println("Meu primeiro codigo em \\Java\\ feito por mim mesmo :)\n");
+		System.out.println("Meu primeiro codigo em Java feito por mim mesmo :)");
+
+	}
+
+}
