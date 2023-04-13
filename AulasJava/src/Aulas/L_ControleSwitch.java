9049161e23eb60dfd942efb7e27afcfcbe17bd68
Henrique Siegel <henriqueweirichmeurersiegel@gmail.com>
2023-04-13 18:36:11 -0300

    Adicionando novas aulas.

---
+++ AulasJava/src/Aulas/L_ControleSwitch.java
@@ -0,0 +1,29 @@
+package Aulas;
+
+import java.util.Scanner;
+
+public class L_ControleSwitch {
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Digite o final da placa para descobrir o vencimento do seu IPVA --> ");
+		int finalPlaca = entrada.nextInt();
+		
+		switch (finalPlaca) {
+		case 1:	
+			System.out.println("Vencimento dia 11 de Janeiro");
+			break; // O "break" é usado para o "switch" parar de escrever os cases seguintes. 
+		case 2:	   // Sem o "breake" a switch vai ler todos os cases, escrevendo-os.
+			System.out.println("Vencimento dia 12 de Janeiro");
+			break;
+		default: // se não for o "case 1" e nem o "case 2", ele vai pro default.
+			System.out.println("Vencimento sem data, desculpe o transtorno!");
+			
+		}
+		
+		
+
+	}
+
+}
---
+++ AulasJava/src/Aulas/M_OperadorTernario.java
@@ -0,0 +1,32 @@
+package Aulas;
+
+import java.util.Scanner;
+
+public class M_OperadorTernario {
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Digite sua idade --> ");
+		int idade = entrada.nextInt();
+		
+		//                 expressão booleana			 valor caso verdadeiro				    valor caso falso
+		// String indicaçao = (idade >= 18)           ? 	"Você é adulto!"               :      "Você é criança/adolescente";
+	
+		///	int x = (idade >= 18) ? 1 : 2;    // Funciona não só para casos booleanos, mas também para outros casos, como o "int", por exemplo
+		
+		
+		if ((idade >= 18) && (idade < 65)) {
+			System.out.println("Você é adulto!");
+		} else if (idade < 18) {
+			System.out.println("Você é criança/adolescente!");
+		} else {
+			System.out.println("Você é um idoso!");
+		}
+		
+		//System.out.println("Resultado: " + indicaçao);
+		
+
+	}
+
+}
