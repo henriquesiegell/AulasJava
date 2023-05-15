d720818dfae3312cf920b4d4ac196cb7af68152d
Henrique Siegel <henriqueweirichmeurersiegel@gmail.com>
2023-05-15 19:29:44 -0300

    Atualização.

---
+++ AulasJava001/src/A_FundamentosDaLinguagem/A_B_C.java
@@ -0,0 +1,15 @@
+package A_FundamentosDaLinguagem;
+
+import java.util.Scanner;
+
+public class A_B_C {
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		
+		
+
+	}
+
+}
