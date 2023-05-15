c45d14e0ac8388bb828e1dd5da620c87fcdc1601
Henrique Siegel <henriqueweirichmeurersiegel@gmail.com>
2023-05-15 16:50:00 -0300

    Revert "Delete AulasJava/src/A_FundamentosDaLinguagem directory"
    This reverts commit deefb1f16b17bdcb6f71a788a8bfbb0ed09fa341.

---
+++ AulasJava/src/A_FundamentosDaLinguagem/O_ControleWhile.java
@@ -0,0 +1,29 @@
+package A_FundamentosDaLinguagem;
+
+import java.util.Scanner;
+
+public class O_ControleWhile {
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Digite o número inicial: ");
+		int numeroInicial = entrada.nextInt();
+		
+		System.out.print("Digite o número final: ");
+		int numeroFinal = entrada.nextInt();
+		
+		int numeroAtual = numeroInicial;
+		
+		while(numeroAtual <= numeroFinal) {
+			System.out.println(numeroAtual);
+			numeroAtual++;
+		}
+		
+		
+		
+			
+
+	}
+
+}
---
+++ AulasJava/src/A_FundamentosDaLinguagem/Q_ControleFor.java
@@ -0,0 +1,23 @@
+package A_FundamentosDaLinguagem;
+
+import java.util.Scanner;
+
+public class Q_ControleFor {
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		
+		System.out.print("Ultimo número: ");
+		int numeroFinal = entrada.nextInt();
+		
+		// for (iniciação;      condição;         incremento)
+		for (int  i = 1;     i <= numeroFinal;        i++) {
+			System.out.println(i);
+		}
+		
+			
+
+	}
+
+}
