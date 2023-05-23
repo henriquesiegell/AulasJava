234df8cb8c7f592534d36fca45bf43d327f39619
Henrique Siegel <henriqueweirichmeurersiegel@gmail.com>
2023-05-22 21:54:23 -0300

    Atualização.

--- AulasJava02/src/G_ContaBanco/AContaBanco.java
+++ AulasJava02/src/G_ContaBanco/AContaBanco.java
@@ -2,8 +2,6 @@ package G_ContaBanco;
 
 import java.util.Scanner;
 
-
-
 public class AContaBanco {
 	Scanner entrada = new Scanner(System.in);
 	
@@ -12,78 +10,159 @@ public class AContaBanco {
 	protected int tipo;
 	private String dono;
 	private float saldo;
+	private float pagMensal;
 	private boolean status;
+		
+	
+	//Construtor
+	public AContaBanco(float saldo, boolean status) {
+			this.saldo = 0;
+			this.status = false;
+	}
+	
 	
 	//Criação de Métodos
-	public Object abrirConta(){
-		System.out.print("Digite 1 para criar CC ou digite 2 para criar CP: ");
-		int tipoCCCP = entrada.nextInt();
+	public void estadoAtual() {
+		System.out.println("---------------------------------------------");
+		System.out.println("Conta: " + this.getNumConta());
+		System.out.println("Tipo: " + this.getTipo());
+		System.out.println("Dono: " + this.getDono());
+		System.out.println("Saldo: R$" + getSaldo());
+		System.out.println("Status: " + isStatus());
+		System.out.println("---------------------------------------------");
 		
-		if (tipoCCCP == 1) {
+	}
+	
+	
+	public Object abrirConta(){		
+		System.out.print(this.dono + ", digite 1 para criar CC ou digite 2 para criar CP: ");
+		int tipoCCCP = entrada.nextInt();
+		System.out.println();
+		
+		if (tipoCCCP == 1) {						
 			System.out.println("Conta Corrente criada com sucesso!");
-			this.status = true; // Para conta aberta, o status é true
+			System.out.println();
+	
+			this.setStatus(true); // Para conta aberta, o status é true
+			this.setSaldo(50);
+			this.setTipo(1);
+			
+			
+			
 			
 		}  else if (tipoCCCP == 2) {
-			System.out.println("Conta Poupança criada com sucesso!");
+			System.out.println("Conta Poupanca criada com sucesso!");			
+			System.out.println();
+			
 			this.status = true; // Para conta aberta, o status é true
+			this.setSaldo(150);
+			this.setTipo(2);
+			
 			
 		}	else {
 			System.out.println("ERRO! Tente novamente.");
+			System.out.println();
 			return abrirConta(); // caso o usuario erre o numero, ele consegue responder novamente
 		}
 		return tipoCCCP;
-		
-		
 		
 	}
 	
 	
 	public int fecharConta() {
-		System.out.print("Para fechar a conta digite 1 para prosseguir ou 2 para desistir do cancelamento: ");
+		System.out.print(this.dono + ", para fechar a conta digite 1 para prosseguir ou 2 para desistir do cancelamento: ");
 		int fechaConta = entrada.nextInt();
 		
-		if (fechaConta == 1) {
+		if (fechaConta == 1) { // para fechar uma conta não pode ter dinheiro e nem dever na conta
 			
-			
-			if (saldo < 0) {
+			if (this.getSaldo() < 0) {
 				System.out.println("Não podemos fechar, você esta devendo.");
-			} else if (saldo > 0) {
+				System.out.println();
+				this.status = true;
+			} else if (this.getSaldo() > 0) {
 				System.out.println("Não podemos fechar, você tem saldo na conta.");
+				System.out.println();
+				this.status = true;
 			} else {
 				System.out.println("Conta fechada com sucesso!");
+				System.out.println();
 				this.status = false;
 			} 			
 		
 			
-			} else if (fechaConta == 2 ) {
+		} else if (fechaConta == 2 ) {
 				System.out.println("Obrigado por continuar conosco!");
-			} else {
+		} else {
 				System.out.println("ERRO! Tente novamente.");
 				return fecharConta();
-			}
-		return fechaConta;
-			
-		
-		
+		}
+		return fechaConta;	
 	}
-	
+		
 	
 	public void depositar() {
+		System.out.print(this.dono + ", qual valor do deposito? R$");
+		float depositoConta = entrada.nextFloat();
 		
+		if (this.isStatus() == true) {
+			 
+			this.setSaldo(this.getSaldo() + depositoConta);
+			System.out.println("Deposito realizado com sucesso! Seu saldo agora eh de R$" + this.saldo);
+			
+			
+		} else {
+			System.out.println("Nao da para depositar, voce ainda nao tem conta.");
+		}
 	}
 	
 	
 	public void sacar() {
+		System.out.print(this.dono + ", qual valor do saque: R$");
+		float saque = entrada.nextFloat();
 		
+		if (this.status == true) {
+			if (saque <= getSaldo()) {
+				this.setSaldo(this.getSaldo() - saque);
+				System.out.println("Saque feito com sucesso!");
+
+			} else {
+				System.out.println("ERRO! Saldo insuficiente.");
+			}
+		} else {
+			System.out.println("ERRO! Voce nao tem conta.");
+		}
 	}
 	
 	
 	public void pagarMensal() {
+		if (this.getTipo() == 1) {
+			this.setPagMensal(this.getSaldo() - 12);
+			System.out.println("O valor do pagamento eh de R$12,00 e o saldo ficou em R$" + this.saldo );
+			System.out.println();
+			
+		} else  {
+			this.setPagMensal(this.getSaldo() - 20);
+			System.out.println("O valor do pagamento eh de R$20,00 e o saldo ficou em R$" + this.saldo);
+			System.out.println();
+		} 
+	}
+	
+	
+	public void status() {
+		if (this.status == false) {
+			System.out.println("Sua conta esta fechada!");
+			System.out.println();
+		} else {
+			System.out.println("Sua conta esta aberta!");
+			System.out.println();
+		}
+		
 		
 	}
 	
-	public void status() {
-		System.out.println("A sua conta está aberta? " + this.status);
+	
+	public void numConta() {
+		System.out.println("O numero da sua conta eh " + this.numConta);
 	}
 	
 	
@@ -121,16 +200,47 @@ public class AContaBanco {
 		this.saldo = saldo;
 	}
 
+
 	public boolean isStatus() {
 		return status;
 	}
 
+
 	public void setStatus(boolean status) {
 		this.status = status;
 	}
+
+
+	public float getPagMensal() {
+		return pagMensal;
+	}
+
+
+	public void setPagMensal(float pagMensal) {
+		this.pagMensal = pagMensal;
+	}
+
+
+	public int getTipo() {
+		return tipo;
+	}
 	
 	
-	//Construtor
+	
+
+	
+	
+	
+	
+
+	
+
+		
+		
+	}
+	
+	
+	
 	
 	
 	
@@ -144,4 +254,4 @@ public class AContaBanco {
 	
 	
 
-}
+
--- AulasJava02/src/G_ContaBanco/BContaBanco.java
+++ AulasJava02/src/G_ContaBanco/BContaBanco.java
@@ -1,12 +1,37 @@
 package G_ContaBanco;
 
+import java.util.Scanner;
+
 public class BContaBanco {
 
 	public static void main(String[] args) {
-		AContaBanco conta1 = new AContaBanco();
+		Scanner entrada = new Scanner(System.in);
 		
-		conta1.fecharConta();
-		conta1.status();
+		AContaBanco p1 = new AContaBanco(0, false);
+		p1.setDono("Lilia");
+		p1.setNumConta(154172);
+		p1.abrirConta();
+		
+		AContaBanco p2 = new AContaBanco(0, false);
+		p2.setDono("Gabriela");
+		p2.setNumConta(895641);
+		p2.abrirConta();	
+		
+		
+		p1.depositar();
+		p2.depositar();
+		
+		p2.sacar();
+		p2.fecharConta();
+				
+		p1.estadoAtual();
+		p2.estadoAtual();
+		
+		
+
+		
+		
+		
 
 	}
 
